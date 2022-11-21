8376c167f323563a00d76880092906cf91b338e2
laurent <laurentbaltra@gmail.com>
2022-11-21 13:05:22 +0100

    add class car

---
+++ main.py
@@ -0,0 +1,9 @@
+class car:
+    def __init__(self,modele,marque):
+        self.modele=modele,
+        self.marque=marque,
+a=car('range_rover','toyota')
+b=car('land_cruiser','mazda')
+print(a.modele,a.marque)
+print(b.modele,b.marque)
+        
